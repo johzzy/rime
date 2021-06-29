cafde126041942a9f992059db8846f9788ad47f3
Johnny <hellojinqiang@gmail.com>
2021-06-29 20:58:41 +0800

    feat(clover): setup clover.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 Root=$(cd $(dirname $0); pwd)
 Source=${Root}/配置文件
+Clover=${Root}/clover
 Target=$(cd ~/Library/Rime; pwd)
 
 function print {
@@ -10,6 +11,7 @@ function print {
 }
 
 function deploy {
+    cp -r ${Clover}/* ${Target}
     cp -r ${Source}/* ${Target}
     /Library/Input\ Methods/Squirrel.app/Contents/MacOS/Squirrel --reload
 }
